acaedb9d7d5804ccac3017301513cbecbf5a24f3
wiranda.srb <wiranda.srb@gmail.com>
2025-11-14 21:11:55 +0700

    fix: default scale bar label

--- src/contants/scaleBarLabel.js
+++ src/contants/scaleBarLabel.js
@@ -1,7 +1,7 @@
 export const defaultScaleLabels = [
-    { id: 1, label: "น้อยที่สุด", value: 1 },
-    { id: 2, label: "น้อย", value: 2 },
-    { id: 3, label: "ปานกลาง", value: 3 },
-    { id: 4, label: "มาก", value: 4 },
-    { id: 5, label: "มากที่สุด", value: 5 },
+    { temp_id: 1, label: "น้อยที่สุด", value: 1 },
+    { temp_id: 2, label: "น้อย", value: 2 },
+    { temp_id: 3, label: "ปานกลาง", value: 3 },
+    { temp_id: 4, label: "มาก", value: 4 },
+    { temp_id: 5, label: "มากที่สุด", value: 5 },
 ];
